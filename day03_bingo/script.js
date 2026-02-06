fe85dedf0665eb973ce3645056b9c6d9c664ab34
Ayuna Maruko <ayuna@Mac.lan>
2026-02-07 08:34:18 +0900

    新しいビンゴにのみアラートが出るようにした

--- day03_bingo/script.js
+++ day03_bingo/script.js
@@ -1,6 +1,8 @@
 const board = document.getElementById("bingo-board");
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"];
 board.innerHTML = "";
+
+// ビンゴのパターンと、それぞれのパターンが出たかどうかを格納する配列を作る
 const bingoPatterns = [
   [0,1,2], // 横1行目
   [3,4,5], // 横2行目
@@ -11,8 +13,7 @@ const bingoPatterns = [
   [0,4,8], // 斜め左上→右下
   [2,4,6]  // 斜め右上→左下
 ];
-
-
+let bingoAchieved=Array(bingoPatterns.length).fill(false);
 
 
 // lettersをシャッフル（Fisher-Yatesアルゴリズム）
@@ -28,26 +29,25 @@ bingoLetters[4] = "FREE";
 // ビンゴ判定
 function checkBingo(){
     const cells=document.querySelectorAll(".cell");
-    return bingoPatterns.some(pattern=>{
-        return pattern.every(index=>{
-            const cell=cells[index];
-            return cell.classList.contains("selected");
-        });
+    let newBingo=false;
+
+    bingoPatterns.forEach((pattern, index)=>{//patternはbingoPatternの要素、indexはbingoAchievedの番号
+        if(!bingoAchieved[index]){//あるパターンのビンゴがまだ出ていなければ
+            const isBingo=pattern.every(i=>//.everyは配列のすべての要素が条件を満たすかを判定。
+                cells[i].classList.contains("selected")
+            );
+        if(isBingo){
+            bingoAchieved[index]=true;//あるパターンのビンゴが出た、とフラグを立てて
+            newBingo=true;//新しくビンゴが出たということにする
+        } 
+        }
     });
 
-
-/*
-    if(
-        cells[0].classList.contains("selected")&&
-        cells[1].classList.contains("selected")&&
-        cells[2].classList.contains("selected")
-    ){
-        return true;
-    }else{
-        return false;
-    }
-*/
+    return newBingo;//新しくビンゴが出たかどうか
 };
+
+
+
 
 
 // cellを作り、letterを入れる
