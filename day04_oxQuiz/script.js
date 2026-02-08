2e13852ec89ca88cd8290cc98d96e6b7ab25b188
ayu-marco <b1023245@fun.ac.jp>
2026-02-08 14:08:46 +0900

    クイズを複数追加し、次のクイズに答えられるようになった

--- day04_oxQuiz/script.js
+++ day04_oxQuiz/script.js
@@ -1,55 +1,103 @@
-const quizData=[
+const quizData = [
   {
-    question:"日本で一番高い山は富士山である。",
-    correct:true,
-    description:"日本で一番高い山は富士山です。"
-  }
+    question: "わからないことがあったら、おうちの人に相談します。",
+    correct: true,
+    description:
+      "わからないことがあったら、そのままにしないで、すぐにおうちの人や先生に相談しよう。",
+  },
+  {
+    question: "自分1人のときに、プレゼントに応募してもいい。",
+    correct: false,
+    description:
+      "個人情報はとても大切なものなので、必ず大人の人と一緒に応募しましょう。",
+  },
+  {
+    question: "掲示板やチャットで書くことを、おうちの人と相談します。",
+    correct: true,
+    description:
+      "掲示板やチャットに参加する前に、おうちの人とどんなことを書いてもいいのか、決めておきましょう。",
+  },
+  {
+    question: "好きな友達の名前を掲示板に書いてもいい。",
+    correct: false,
+    description:
+      "友達の名前も個人情報です。友達の名前を書くときは、ニックネームや下の名前だけにしましょう。",
+  },
+  {
+    question:
+      "掲示板やチャットなどでは、ハンドルネームやニックネームを使います。",
+    correct: true,
+    description: "本当の名前は個人情報なので教えてはいけません。",
+  },
+  {
+    question:
+      "インターネットを通じて仲良くなった友達でも、本当の名前を教えません。",
+    correct: true,
+    description:
+      "本当は悪い大人の人かもしれないので、どんなに仲良しでも教えてはいけません。",
+  },
 ];
-const qNumber=document.getElementById("quiz-number");
-const qText=document.getElementById("quiz-question");
-const judge=document.getElementById("judge");
-const aCorrect=document.getElementById("answer-correct");
-const aText=document.getElementById("answer-comment");
+const qNumber = document.getElementById("quiz-number");
+const qText = document.getElementById("quiz-question");
+const judge = document.getElementById("judge");
+const aCorrect = document.getElementById("answer-correct");
+const aText = document.getElementById("answer-comment");
+const next = document.getElementById("next-btn");
 
-let currentStep=0;
+let currentStep = 0;
+let score=0;
 
-function correctAnswer(torf){
-  if(torf){
-    aCorrect.textContent="正解は○";
-  }else{
-    aCorrect.textContent="正解は×";
+function correctAnswer(torf) {
+  if (torf) {
+    aCorrect.textContent = "正解は○";
+  } else {
+    aCorrect.textContent = "正解は×";
   }
-};
+}
 
-function displayQuiz(){
-  const currentQuiz=quizData[currentStep];
+function displayQuiz() {
+  const currentQuiz = quizData[currentStep];
 
-  qNumber.textContent=`第${currentStep+1}問`
-  qText.textContent=currentQuiz.question;
+  qNumber.textContent = `第${currentStep + 1}問`;
+  qText.textContent = currentQuiz.question;
   correctAnswer(currentQuiz.correct);
-  aText.textContent=currentQuiz.description;
+  aText.textContent = currentQuiz.description;
 }
 displayQuiz();
 
-document.getElementById("true-btn").addEventListener("click", ()=>{
+document.getElementById("true-btn").addEventListener("click", () => {
   checkAnswer(true);
 });
-document.getElementById("false-btn").addEventListener("click", ()=>{
+document.getElementById("false-btn").addEventListener("click", () => {
   checkAnswer(false);
 });
 
-function checkAnswer(userChoice){
-  const currentQuiz=quizData[currentStep];
+function checkAnswer(userChoice) {
+  const currentQuiz = quizData[currentStep];
 
   document.getElementById("quiz").classList.add("hidden");
   document.getElementById("answer").classList.remove("hidden");
-  if(userChoice===currentQuiz.correct){
-    judge.textContent="正解！";
-  }else{
-    judge.textContent="残念…";
+  if (userChoice === currentQuiz.correct) {
+    judge.textContent = "正解！";
+    score++;
+  } else {
+    judge.textContent = "残念…";
   }
 }
 
+next.addEventListener("click", () => {
+  currentStep++;
+
+  if (currentStep < quizData.length) {
+    document.getElementById("quiz").classList.remove("hidden");
+    document.getElementById("answer").classList.add("hidden");
+    displayQuiz();
+  }else{
+    alert("全問終了しました！");
+    document.getElementById("quiz-screen").classList.add("hidden");
+    document.getElementById("result-screen").classList.remove("hidden");
+  }
+});
 
 /*
 const board = document.getElementById("bingo-board");
