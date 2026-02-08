d0dd4c8933ce12f3c9c95363df8845b585bfe369
ayu-marco <b1023245@fun.ac.jp>
2026-02-08 20:47:19 +0900

    AIvsハッカーのイラストを追加

--- day04_oxQuiz/script.js
+++ day04_oxQuiz/script.js
@@ -46,9 +46,11 @@ const next = document.getElementById("next-btn");
 const start = document.getElementById("start-btn");
 const restart=document.getElementById("restart-btn");
 
+
 let currentStep = 0;
 let score = 0;
 
+// スタートボタンを押した時
 start.addEventListener("click", () => {
   currentStep = 0;
   score = 0;
@@ -59,6 +61,7 @@ start.addEventListener("click", () => {
   displayQuiz();
 });
 
+// 正解かどうかで表示する文章を変える
 function correctAnswer(torf) {
   if (torf) {
     aCorrect.textContent = "正解は○";
@@ -67,23 +70,31 @@ function correctAnswer(torf) {
   }
 }
 
+// クイズを表示
 function displayQuiz() {
   const currentQuiz = quizData[currentStep];
-
   qNumber.textContent = `第${currentStep + 1}問`;
   qText.textContent = currentQuiz.question;
   correctAnswer(currentQuiz.correct);
   aText.textContent = currentQuiz.description;
-}
-//displayQuiz();
+document.getElementById("lose").classList.remove("hidden");
+document.getElementById("e-lose").classList.remove("hidden");
+document.getElementById("win").classList.remove("hidden");
+document.getElementById("e-win").classList.remove("hidden");
 
+
+}
+
+// oボタンを押したとき
 document.getElementById("true-btn").addEventListener("click", () => {
   checkAnswer(true);
 });
+// xボタンを押したとき
 document.getElementById("false-btn").addEventListener("click", () => {
   checkAnswer(false);
 });
 
+// ユーザーの解答が正しいかどうか判別しメッセージを表示
 function checkAnswer(userChoice) {
   const currentQuiz = quizData[currentStep];
 
@@ -91,12 +102,17 @@ function checkAnswer(userChoice) {
   document.getElementById("answer").classList.remove("hidden");
   if (userChoice === currentQuiz.correct) {
     judge.textContent = "正解！";
+    document.getElementById("lose").classList.add("hidden");
+    document.getElementById("e-lose").classList.add("hidden");
     score++;
   } else {
     judge.textContent = "残念…";
+    document.getElementById("win").classList.add("hidden");
+    document.getElementById("e-win").classList.add("hidden");
   }
 }
 
+// ネクストボタンを押したとき
 next.addEventListener("click", () => {
   currentStep++;
 
@@ -109,6 +125,8 @@ next.addEventListener("click", () => {
     showResult();  
   }
 });
+
+// 全問終了したとき
 function showResult(){
   document.getElementById("quiz-screen").classList.add("hidden");
     document.getElementById("result-screen").classList.remove("hidden");
@@ -117,6 +135,7 @@ function showResult(){
     report.textContent=`${quizData.length}問中 ${score}問 正解！`;
 };
 
+// スタートへ戻るボタンを押したとき
 restart.addEventListener("click", ()=>{
   document.getElementById("result-screen").classList.add("hidden");
     document.getElementById("start-screen").classList.remove("hidden");
