4e7d1640dd291644e8fe3a18cdc60e04780669a7
ayu-marco <b1023245@fun.ac.jp>
2026-02-09 21:11:57 +0900

    切り替えボタンとシャッフルボタンを分けた

--- day05_letterCards/script.js
+++ day05_letterCards/script.js
@@ -1,69 +1,77 @@
 const board = document.getElementById("card-board");
-const uL = ["A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",];
-const lL = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-const btn = document.getElementById("switch");
+const wordData = {
+  upper: [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ],
+  lower: [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ],
+};
+const switchBtn = document.getElementById("switch");
+const shuffleBtn = document.getElementById("shuffle");
 board.innerHTML = "";
+let currentMode = "upper";
+let letters = [...wordData[currentMode]];
 
-let letters = uL;
-
-function setLetters() {
+function shuffleLetters() {
   board.innerHTML = "";
   for (let i = letters.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [letters[i], letters[j]] = [letters[j], letters[i]];
   }
+}
+function renderBoard() {
+  board.innerHTML = "";
 
   letters.forEach((letter, index) => {
     const cell = document.createElement("div");
@@ -76,16 +84,25 @@ function setLetters() {
 
     board.appendChild(cell); //boardの子として追加する
   });
-};
-setLetters();
-btn.addEventListener("click", () => {
-  if (letters == uL) {
-    letters = lL;
-    btn.textContent = "大文字バージョンにする";
-    setLetters();
+}
+
+switchBtn.addEventListener("click", () => {
+  if (currentMode === "upper") {
+    currentMode = "lower";
+    letters = [...wordData.lower];
+    switchBtn.textContent = "大文字にする";
   } else {
-    letters = uL;
-    btn.textContent = "小文字バージョンにする";
-    setLetters();
+    currentMode = "upper";
+    letters = [...wordData.upper];
+    switchBtn.textContent = "小文字にする";
   }
+  renderBoard();
 });
+
+shuffleBtn.addEventListener("click", () => {
+  shuffleLetters();
+  renderBoard();
+});
+
+shuffleLetters();
+renderBoard();
