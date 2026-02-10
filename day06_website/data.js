742e919d474b811d6388b728d553023018de164f
ayu-marco <b1023245@fun.ac.jp>
2026-02-10 21:33:31 +0900

    画像やリンクを本物にした

--- day06_website/data.js
+++ day06_website/data.js
@@ -1,14 +1,38 @@
 export const appList = [
   { 
-    title: "計算アプリ", 
-    desc: "算数の練習ができます",
-    link: "https://google.com", // 実際のURLに
-    image: "https://via.placeholder.com/250x150" // 仮の画像URL
+    title: "九九カード", 
+    desc: "",
+    link: "https://view.genially.com/6959d78868f005afdae6b02b/interactive-content-", // 実際のURLに
+    image: "images/九九カード.png" // 仮の画像URL
   },
   { 
-    title: "英単語アプリ", 
+    title: "九九カード②", 
+    desc: "",
+    link: "https://view.genially.com/6959bedbaf1831ae6ecb3a8a/interactive-content-", // 実際のURLに
+    image: "images/九九カード②.png" // 仮の画像URL
+  },
+  { 
+    title: "好きなもの紹介カード", 
+    desc: "",
+    link: "https://view.genially.com/695a26ebaf1831ae6edde7ad/interactive-content-like", // 実際のURLに
+    image: "images/好きなもの紹介カード.png" // 仮の画像URL
+  },
+  { 
+    title: "小文字カード", 
+    desc: "",
+    link: "https://view.genially.com/695aff1b68f005afda1be0a1/interactive-content-", // 実際のURLに
+    image: "images/小文字カード.png" // 仮の画像URL
+  },
+  { 
+    title: "大文字カード", 
+    desc: "",
+    link: "https://view.genially.com/695b63410205f1624b6e5dfc/interactive-content-", // 実際のURLに
+    image: "images/大文字カード.png" // 仮の画像URL
+  },
+  { 
+    title: "並べ替えカード", 
     desc: "英語の勉強ができます",
-    link: "#",
-    image: "https://via.placeholder.com/250x150"
+    link: "https://view.genially.com/69880744dea8f5798eae110d/interactive-content-",
+    image: "images/並べ替えカード.png"
   }
 ];
